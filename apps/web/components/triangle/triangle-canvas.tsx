8a77916a39166dcbe26d5fba505c921a80f2d140
dhanushd-27 <dhanushsai2005@gmail.com>
2026-01-25 20:49:26 +0530

    Add a back button in triangle canvas

--- apps/web/components/triangle/triangle-canvas.tsx
+++ apps/web/components/triangle/triangle-canvas.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Button } from '@repo/ui/button'
+import { Shape, useCurrentShapeStore } from '../../stores/currentShape-store'
 
 export default function TriangleCanvas() {
+  const setShape = useCurrentShapeStore((state) => state.setShape)
+
   return (
-    <div className='text-xs'>Coming Soon!</div>
+    <div className='flex items-center justify-center relative flex-col gap-4'>
+      <div className='text-xs'>Coming Soon!</div>
+      <Button variant='text' onClick={() => setShape(Shape.None)}>Back</Button>
+    </div>
   )
 }
